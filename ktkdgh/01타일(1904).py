78a04c57f02fb547886d6173d1ad28886a984a27
ktkdgh <96051392+ktkdgh@users.noreply.github.com>
2022-04-22 20:39:30 +0900

    Create 01타일(1904).py

---
+++ ktkdgh/01타일(1904).py
@@ -0,0 +1,13 @@
+#바텀업 사용
+import sys
+n = int(sys.stdin.readline())
+d = [0] * (n+1)
+
+if n == 1:
+    print(1)
+else:
+    d[1] = 1
+    d[2] = 2
+    for i in range(3, n+1):
+        d[i] = (d[i-1] + d[i-2]) %15746
+    print(d[n])
